2a8ecb6f46a9b42f06fc11d1c9ca867279adb630
Casey Overton <overtonc1@gmail.com>
2021-02-14 02:47:18 -0500

    complete contact page

--- src/components/Page/ContactPage.js
+++ src/components/Page/ContactPage.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
 import image from '../../assets/Img/contact.png'
-
+import Button from '@material-ui/core/Button'
 function ContactPage(props) {
     return (
-        <div>
-            <h1>CONTACT</h1>
-            <Paper img= {image} variant="outlined">
-             
-            </Paper>
+        <div class='contact'>
+           
+            <div class="text">
+             <h1>Casey Overton</h1><br></br>
+             <h3>Email: overtonc1@gmail.com</h3>
+             <h3>Phone: 919-482-5678</h3>
+             <Button href="https://github.com/CaseyOverton" size="small" color="primary">
+               Github Profile
+             </Button>
+             <Button href="https://www.linkedin.com/in/casey-overton-3584151b1/" size="small" color="primary">
+               Linkedin
+             </Button>
+             </div>
         </div>
     )
 }
