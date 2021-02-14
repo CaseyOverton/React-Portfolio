b1a882e0c84248af3445b0cc6097ca9a5985bfa9
Casey Overton <overtonc1@gmail.com>
2021-02-14 02:55:44 -0500

    got resume rendering

--- src/components/Page/Resume.js
+++ src/components/Page/Resume.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Resume(props) {
     return (
-        <div>
-            <h1>I WORK!!!!</h1>
+        <div class="resume">
+            
         </div>
     )
 }
